a6536ff7dc60060bf4a4c4208adfec5ace4bd010
alanolv <olvera.cristino.alan@gmail.com>
2024-02-04 00:21:50 -0600

    Feat:Se agregan estilos y secciones al CV

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import {useFonts} from 'expo-font'
 import { SafeAreaView , StyleSheet, Text, View , Image} from 'react-native';
 
 const App = () => {
@@ -16,12 +15,34 @@ const App = () => {
         </View>
         <View style={style.hName}>
           <Text style={style.txtHeader}>Alan Olvera Cristino</Text>
-          <Text style={[style.txtHeader,{color: "#9c88ff"}]}>INGENIERO DE SOFTWARE</Text>
+          <Text style={[style.txtHeader,{color: "#a8a8f6",fontSize:14}]}>ING. DE SOFTWARE</Text>
         </View>
       </View>
       <View style={style.bd}>
-        <View style={style.bdLeft}><Text >Left</Text></View>
-        <View style={style.bdRight}><Text>right</Text></View>
+        <View style={style.bdLeft}>
+          <View>
+            <Text style={style.bdTitle}>CONTACTO</Text>
+            <Text style={style.bdLine}>────</Text>
+          </View>
+          <View>
+            <Text style={style.bdTitle}>HABILIDADES</Text>
+            <Text style={style.bdLine}>────</Text>
+          </View>
+          <View>
+            <Text style={style.bdTitle}>EDUCACIÓN</Text>
+            <Text style={style.bdLine}>────</Text>
+          </View>
+        </View>
+        <View style={style.bdRight}>
+          <View>
+            <Text style={style.bdTitle}>ACERCA DE MI</Text>
+            <Text style={style.bdLine}>────</Text>
+          </View>
+          <View>
+            <Text style={style.bdTitle}>EXPERIENCIA LABORAL</Text>
+            <Text style={style.bdLine}>────</Text>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -34,7 +55,8 @@ const style = StyleSheet.create({
   },
   txtHeader:{
     color:'#f5f6fa',
-    fontSize: 18,
+    fontSize: 26,
+    fontWeight:'bold',
     marginTop:10,
   },
   header: {
@@ -66,13 +88,24 @@ const style = StyleSheet.create({
   bdLeft:{
     padding:15,
     width:'45%',
-    backgroundColor:'#dcdde1'
+    backgroundColor:'#e9e9e9'
 
   },
   bdRight:{
     padding:15,
     width:'55%',
-    backgroundColor:'#f5f6fa'
+    backgroundColor:'#eeeeee'
+  },
+  bdTitle:{
+    fontSize:16,
+    fontWeight: 'bold',
+    color:'#291295',
+  },
+  bdLine:{
+    fontSize:20,
+
+    fontWeight: 'bold',
+    color:'#291295',
   }
   
 });
